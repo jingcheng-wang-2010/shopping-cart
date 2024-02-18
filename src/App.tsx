18128425c64933a67ebb02eaec9b8f60f6b7b3da
Jingcheng Wang <jingwang49@gmail.com>
2024-02-18 14:45:29 +0800

    navbar and shopping button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Container } from "react-bootstrap"
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
 
 
 function App() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+
+        </Routes>
+      </Container>
+    </>
+  )
 }
 
 export default App
